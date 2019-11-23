e9447d72a62b7feeffc72b6840f3eeee1fb90139
Jessica <jesvaks11@gmail.com>
2019-11-23 19:42:01 -0300

    Contacto: Lleva a la app de mail

--- app/src/main/java/com/jnj/cinepop/activities/ContactoActivity.java
+++ app/src/main/java/com/jnj/cinepop/activities/ContactoActivity.java
@@ -1,9 +1,12 @@
 package com.jnj.cinepop.activities;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -35,5 +38,44 @@ public class ContactoActivity extends AppCompatActivity {
         txtComentario = findViewById(R.id.txtComentarios);
         btnSendComentario = findViewById(R.id.btnSendComment);
 
+        btnSendComentario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enviarMail();
+            }
+        });
+
+        //le aplico al campo el tema de esconder teclado al tocar fuera del campo
+        txtComentario.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus){
+                    hideKeyboard(v);
+                }
+            }
+        });
+
     }
+
+    private void enviarMail() {
+
+        String mensaje = txtComentario.getText().toString();
+        String mailcontacto = "contacto@cinepop.com";
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_EMAIL, mailcontacto);
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Comentario para Cine Pop");
+        intent.putExtra(Intent.EXTRA_TEXT, mensaje);
+
+        intent.setType("message/rfc822");
+        startActivity(Intent.createChooser(intent, "Seleccione la app para enviar el mail"));
+
+    }
+
+    //funcion para esconder teclado al tocar fuera del campo enfocado
+    public void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager =(InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
 }
