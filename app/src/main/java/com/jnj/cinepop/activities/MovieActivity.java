151f95f1f3adce954bcc185531350917337657e1
jonysz95 <jonathansztajnhart@gmail.com>
2019-11-24 15:19:32 -0300

    Se agregó la barra superior para las activities de reserva y pelicula

--- app/src/main/java/com/jnj/cinepop/activities/MovieActivity.java
+++ app/src/main/java/com/jnj/cinepop/activities/MovieActivity.java
@@ -5,6 +5,8 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -25,6 +27,17 @@ public class MovieActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
+
+        Toolbar mToolbar = findViewById(R.id.toolbar);
+        mToolbar.setTitle(getString(R.string.title_activity_movie));
+        mToolbar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp);
+
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
         txtTitulo = findViewById(R.id.txttitle);
         ivImage = findViewById(R.id.movieImg);
--- app/src/main/java/com/jnj/cinepop/activities/ReservaActivity.java
+++ app/src/main/java/com/jnj/cinepop/activities/ReservaActivity.java
@@ -3,6 +3,7 @@ package com.jnj.cinepop.activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -36,6 +37,18 @@ public class ReservaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reserva);
+
+        Toolbar mToolbar = findViewById(R.id.toolbar);
+        mToolbar.setTitle(getString(R.string.title_activity_reserva));
+        mToolbar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp);
+
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
         seatManagerDB = new DBSeatManager();
 
         txtPelicula = findViewById(R.id.txtNombrePelicula);
