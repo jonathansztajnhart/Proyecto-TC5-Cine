ea45b87ea3680752fbfb55380696ddea9cf0deb6
nvitale <nvitale@tgv.com.ar>
2019-11-27 08:50:05 -0300

    Se editan las sinopsis de las peliculas

--- app/src/main/java/com/jnj/cinepop/DBAccess/DBMovieManager.java
+++ app/src/main/java/com/jnj/cinepop/DBAccess/DBMovieManager.java
@@ -24,17 +24,17 @@ public class DBMovieManager {
     }
 
     public void loadMovies(Context context){
-        insertMovie(context, "Avengers Endgame", "Sinopsis pelicula", "+13", "avengers_endgame");
-        insertMovie(context, "Angry Birds 2", "Sinopsis pelicula", "ATP", "angry_birds_2");
-        insertMovie(context, "Capitana Marvel", "Sinopsis pelicula", "+13", "capitana_marvel");
-        insertMovie(context, "Pokemon: Detective Pikachu", "Sinopsis pelicula", "ATP", "pokemon_detective_pikachu");
-        insertMovie(context, "Dora y la ciudad perdida", "Sinopsis pelicula", "ATP", "dora_y_la_ciudad_perdida");
-        insertMovie(context, "Dumbo", "Sinopsis pelicula", "ATP", "dumbo");
-        insertMovie(context, "El hijo", "Sinopsis pelicula", "+16", "el_hijo");
-        insertMovie(context, "Entre la razón y la locura", "Sinopsis pelicula", "+13", "entre_la_razon_y_la_locura");
-        insertMovie(context, "Guason", "Sinopsis pelicula", "+18", "guason");
-        insertMovie(context, "La llorona", "Sinopsis pelicula", "+16", "la_llorona");
-        insertMovie(context, "Shazam", "Sinopsis pelicula", "+13", "shazam");
+        insertMovie(context, "Avengers Endgame", "Los Vengadores restantes deben encontrar una manera de recuperar a sus aliados para un enfrentamiento épico con Thanos.", "+13", "avengers_endgame");
+        insertMovie(context, "Angry Birds 2", "Red, Chuck, Bomb y el resto de sus amigos son abordados por un cerdo verde que les pide que se unan para luchar contra una amenaza común.", "ATP", "angry_birds_2");
+        insertMovie(context, "Capitana Marvel", "Una guerrera extraterrestre de la civilización Kree con la ayuda de Nick Fury trata de descubrir los secretos de su pasado mientras aprovecha sus poderes para terminar la guerra.", "+13", "capitana_marvel");
+        insertMovie(context, "Pokemon: Detective Pikachu", "Un joven une fuerzas con el detective Pikachu para desentrañar el misterio detrás de la desaparición de su padre.", "ATP", "pokemon_detective_pikachu");
+        insertMovie(context, "Dora y la ciudad perdida", "Dora junto a su amigo Botas comienzan una aventura en la que deberán salvar a los padres de Dora y resolver el misterio oculto tras una ciudad perdida de oro.", "ATP", "dora_y_la_ciudad_perdida");
+        insertMovie(context, "Dumbo", "El dueño de un circo en aprietos contrata a un hombre y sus dos hijos para cuidar de un elefante recién nacido que puede volar, que pronto ayuda a revitalizar al circo.", "ATP", "dumbo");
+        insertMovie(context, "El hijo", "Una mujer embarazada se aísla cada vez más de su marido, quien posee un comportamiento obsesivo y sobreprotector.", "+16", "el_hijo");
+        insertMovie(context, "Entre la razón y la locura", "El profesor James Murray comienza a recopilar palabras para la primera edición del Oxford English Dictionary a mitad del siglo XIX.", "+13", "entre_la_razon_y_la_locura");
+        insertMovie(context, "Guason", "Una serie de trágicos sucesos harán que Arthur Fleck distorsione considerablemente su visión del mundo convirtiéndolo en un brillante criminal.", "+18", "guason");
+        insertMovie(context, "La llorona", "Los Ángeles, década de 1970. Un legendario fantasma que se oculta en la oscuridad de la noche aterra a los niños.", "+16", "la_llorona");
+        insertMovie(context, "Shazam", "Billy Batson, un astuto joven de 14 años, se transforma en el superhéroe Shazam, pero sus poderes son puestos a prueba cuando se enfrenta al mal.", "+13", "shazam");
     }
 
     public MovieModel getMovie(Context context, int id) {
